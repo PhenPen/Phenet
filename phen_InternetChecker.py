7147e725e56e0b8e99bcd228c0ac8669f5037c64
Phen <thePhenpen@gmail.com>
2025-11-25 22:03:00 +0100

    Basic Internet Checker for Download and upload speed and ping

---
+++ phen_InternetChecker.py
@@ -0,0 +1,23 @@
+import speedtest
+
+st = speedtest.Speedtest()
+
+print("Finding best server...")
+bestServer = st.get_best_server()
+# print(bestServer)   # For debugging
+
+print("Testing download speed...")
+
+# To get download speed
+download_speed = st.download() / 1_000_000  # To convert to Mbps
+
+# To get upload speed
+upload_speed = st.upload() / 1_000_000      # To convert to Mbps
+
+# To get ping
+ping = st.results.ping
+
+print(f"Download Speed: {download_speed:.2f} Mbps") #.2f for 2 decimal places
+print(f"Upload Speed: {upload_speed:.2f} Mbps") #.2f for 2 decimal places
+print(f"Ping: {ping} ms")
+print("Speed test completed.")
